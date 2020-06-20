0164b87e7e10cb7037f95497459dcbb841d4874e
Derrek <dschuurm@ucsd.edu>
2020-06-20 13:31:17 -0700

    added rolling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use std::env;
-
+use rand::Rng;
 
 fn main() {
-
-    println!("Hello, world!");
-
     // checks that a command was given
     if env::args().len() == 1  {
         println!("no input was given!");
         return;
     } 
 
+    let mut rng = rand::thread_rng();
     let mut _path = "";
     let mut inputs = Vec::new();
     let mut i = 0;
@@ -23,13 +21,18 @@ fn main() {
         i += 1;
     }
 
+    let mut roll_tuples = Vec::new();
 
+    // grab all inputs for errors before actually rolling
     for input in inputs  {
-        println!("{}", input);
-        let return_tuple = parse_input(input);
-        println!("numdice = {}", return_tuple.0);
-        println!("dice_type = {}", return_tuple.1);
+        roll_tuples.push(parse_input(input));
+    }
 
+    for tuple in roll_tuples  {
+        println!("rolling for D{}", tuple.1);
+        for i in 0..tuple.0  {
+            println!("{}", rng.gen_range(1, tuple.1 + 1));
+        }
     }
 
 }
@@ -50,16 +53,14 @@ fn parse_input(input: String) -> (u32, u32)  {
 
       //check if char is an integer - no need to check lower as we are using u32
       if converted > 9 {
-          println!("finished with ints");
-
           //check if we got a number first
           if number_of_dice.len() < 1  {
-              println!("no numbers before char input");
+              println!("error - no numbers before char input");
               return (0, 0);
 
          // triggers if we get non number input after we get dice type
           }  else if dice_type.len() > 1  {
-            println!("too much input");
+            println!("error - too much input");
             return (0, 0);
           }
 
@@ -67,9 +68,8 @@ fn parse_input(input: String) -> (u32, u32)  {
           if d_char_flag == false && (character == 'd' || character == 'D') {
               d_char_flag = true;
               dice_num_flag = true;
-              println!("D is there");
           }  else  {
-              println!("bad input");
+              println!("error - bad input");
           }
 
       //this is still the first set of numbers so we add to numDice
@@ -80,7 +80,6 @@ fn parse_input(input: String) -> (u32, u32)  {
           dice_type.push(converted);
       }
 
-      println!("{}", converted);
     }  //end for loop
     let return_values = (convert_vector_of_u32_to_single_u32(number_of_dice.as_mut_slice()), convert_vector_of_u32_to_single_u32(dice_type.as_mut_slice()));
     return return_values;
