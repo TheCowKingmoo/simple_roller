22a887b97307669b65801f3f2e94825cd81f778f
Derrek <dschuurm@ucsd.edu>
2020-06-22 09:36:54 -0700

    cleanup parse

--- src/parse.rs
+++ src/parse.rs
@@ -1,5 +1,19 @@
 
 
+const ARG_CHAR: char = '-';
+const ARG_AVERAGE_CHAR: char = 'a';
+const DICE_CHAR_LOWER: char = 'd';
+const DICE_CHAR_UPPER: char = 'D';
+const EMPTY_STRING: &str = "";
+const E_TOO_SMALL_INPUT: &str = "input is too small";
+const E_MORE_THAN_ONE_ARG_CHAR: &str = "more than one arg character was detected";
+const E_NON_INT_BEFORE_D: &str = "got a non-integer before D";
+const E_NON_INT_AFTER_D: &str = "got non-integer after D";
+const E_DEFAULT: &str = "default error catcher";
+const E_MULTIPLE_SAME_ARG: &str = "Error - more than one of the same arg detected";
+const E_NO_MATCH_ARG: &str = "Error - no matching arguments";
+
+
 pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, String)  {
 
     const ASCIIFIX: u32 = 48;
@@ -8,7 +22,7 @@ pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, Strin
     let mut arg_str_return: String = String::new();
 
     if message_string.len() < 9  { //prefix + roll + whitespace + 1D1
-      arg_str_return.push_str("input is too small");
+      arg_str_return.push_str(E_TOO_SMALL_INPUT);
       return (0, 0, arg_char_vector, arg_str_return);
     }
 
@@ -30,7 +44,6 @@ pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, Strin
 
       // skip the first loop - always equal to ~roll
       if i == 0  {
-        println!("i");
         i += 1;
         continue;
       }
@@ -39,13 +52,11 @@ pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, Strin
       let mut j = 0;
       // now we break down the string into characters
       for character in chunk_string.chars()  {
-        println!("{}", character);
         // check if we are looking at args
-        if j == 0 && character == '-'  {
-          println!("enter");
+        if j == 0 && character == ARG_CHAR  {
           if operator_flag == true  {
             // we have already tried to pick up arguments
-            arg_str_return.push_str("more than one - was detected");
+            arg_str_return.push_str(E_MORE_THAN_ONE_ARG_CHAR);
             return (0, 0, arg_char_vector, arg_str_return);
 
             }  else  {
@@ -53,7 +64,7 @@ pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, Strin
               let arg_return_tuple = break_up_arg(chunk_string);
               arg_char_vector = arg_return_tuple.0;
               arg_str_return = arg_return_tuple.1;
-              if arg_str_return != ""  {
+              if arg_str_return != EMPTY_STRING  {
                 return (0, 0, arg_char_vector, arg_str_return);
               }
               break;
@@ -67,23 +78,23 @@ pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, Strin
           if converted > 9 {
               //check if we got a number first
               if number_of_dice.len() < 1  {
-                arg_str_return.push_str("got a non-integer before D");
+                arg_str_return.push_str(E_NON_INT_BEFORE_D);
                 return (0, 0, arg_char_vector, arg_str_return);
 
     
              // triggers if we get non number input after we get dice type
               }  else if dice_type.len() > 1  {
-                arg_str_return.push_str("got non-integer after D");
+                arg_str_return.push_str(E_NON_INT_AFTER_D);
                 return (0, 0, arg_char_vector, arg_str_return);
 
               }
     
               //check if this is our single D character
-              if d_char_flag == false && (character == 'd' || character == 'D') {
+              if d_char_flag == false && (character == DICE_CHAR_LOWER || character == DICE_CHAR_UPPER) {
                   d_char_flag = true;
                   dice_num_flag = true;
               }  else  {
-                arg_str_return.push_str("default error catcher");
+                arg_str_return.push_str(E_DEFAULT);
                 return (0, 0, arg_char_vector, arg_str_return);
 
               }
@@ -103,16 +114,14 @@ pub fn parse_roll_message(message_string: String) -> (u32, u32, Vec<char>, Strin
       i += 1;
 
     }
-    println!("got to end");
     return (convert_vector_of_u32_to_single_u32(number_of_dice.as_mut_slice()), convert_vector_of_u32_to_single_u32(dice_type.as_mut_slice()), arg_char_vector, arg_str_return);
 }
 
 fn break_up_arg(arg_string: &str) -> (Vec<char>, String)  {
-  println!("breakup arg");
   let mut i = 0;
   let mut return_vector = Vec::new();
   let mut a_flag = false;
-  let mut return_str = "";
+  let mut return_str = EMPTY_STRING;
   let mut return_string = String::new();
 
   for character in arg_string.chars()  {
@@ -121,17 +130,19 @@ fn break_up_arg(arg_string: &str) -> (Vec<char>, String)  {
       continue;
     }
 
-    match character  {
+    let lower_character = character.to_ascii_lowercase();
+
+    match lower_character  {
       // a = avg
-      'a' => if a_flag == false  {
+      ARG_AVERAGE_CHAR => if a_flag == false  {
         a_flag = true;
         return_vector.push(character);
       }  else  {
-        return_str = "Error - too many a arguments";
+        return_str = E_MULTIPLE_SAME_ARG;
         break;
       },
       // default catch all
-      _ => return_str = "Error - no matching arguments"
+      _ => return_str = E_NO_MATCH_ARG
     }
     
     i+=1;
